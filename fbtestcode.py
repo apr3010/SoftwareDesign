a2ead347cbd7c773da4a1db429608595034f435d
Abigail Rodrigues <apr3010@gmail.com>
2014-09-30 09:27:20 -0400

    test code during process of writing code

---
+++ fbtestcode.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 29 21:14:11 2014
+
+@author: abigail
+"""
+
+sentence = 'Hi my name is Abi'
+#words=sentence.split()
+#print(words)
+#for word in words:
+#    if word== 'my':
+#        print 'yes'
+        
+print sentence.find('my')
